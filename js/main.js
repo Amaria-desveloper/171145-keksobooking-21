7985ccd3d832853e251e31d4be59da3384e70839
Amaria-desveloper (Maria Aleksandrova) <amaria.desveloper@gmail.com>
2020-10-07 17:14:11 +0300

    данные элемента через функцию

--- js/main.js
+++ js/main.js
@@ -2,16 +2,12 @@
 
 const NUMBER_OF_AVATARS = 8;
 
-const widthMap = document.querySelector(`.map`)
-    .offsetWidth;
-const widthPin = document.querySelector(`.map__pin`)
-    .offsetWidth;
-const heightPin = document.querySelector(`.map__pin`)
-    .offsetHeight;
+/*
+
 
 const OFFSET_X = widthPin / 2;
 const OFFSET_Y = heightPin / 2;
-
+*/
 const TYPES = [
   `palace`,
   `flat`,
@@ -26,17 +22,28 @@ const CAPACITY = [
   `не для гостей`
 ];
 
-const TIMEIN = [
+const TIME_IN = [
   `12:00`,
   `13:00`,
   `14:00`
 ];
 
-const TIMEOUT = TIMEIN;
+const TIME_OUT = TIME_IN;
 
-const FEATURES = `wifi, dishwasher, parking, washer, elevator, conditioner`;
+const FEATURES = [
+  `wifi`,
+  `dishwasher`,
+  `parking`,
+  `washer`,
+  `elevator`,
+  `conditioner`
+];
 
-const PHOTOS = `http://o0.github.io/assets/images/tokyo/hotel1.jpg, http://o0.github.io/assets/images/tokyo/hotel2.jpg, http://o0.github.io/assets/images/tokyo/hotel3.jpg`;
+const PHOTOS = [
+  `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
+];
 
 
 const getRandomInteger = function (min, max) {
@@ -47,11 +54,10 @@ const getRandomArrIndex = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-const getRandomArrFromStr = function (stringData) {
-  let arrFromString = stringData.split(`,`);
-  let randNum = getRandomInteger(0, arrFromString.length - 1);
-  let randomArrFromStr = arrFromString.slice(randNum);
-  return randomArrFromStr;
+const getRandomArr = function (arr) {
+  let randNum = getRandomInteger(0, arr.length - 1);
+  let randomArr = arr.slice(randNum);
+  return randomArr;
 };
 
 
@@ -62,6 +68,18 @@ const getAvatars = function () {
   }
   return avatars;
 };
+
+
+const getSizeOfElement = function () {
+  return {
+    Pin: {
+      Width: document.querySelector(`.map__pin`).offsetWidth,
+      Height: document.querySelector(`.map__pin`).offsetHeight
+    },
+    WidthMap: document.querySelector(`.map`).offsetWidth,
+  };
+};
+let sizeOfElement = getSizeOfElement();
 
 
 const getAdvert = function () {
@@ -76,48 +94,46 @@ const getAdvert = function () {
       "type": getRandomArrIndex(TYPES),
       "rooms": getRandomInteger(1, 5),
       "guests": getRandomArrIndex(CAPACITY),
-      "checkin": getRandomArrIndex(TIMEIN),
-      "checkout": getRandomArrIndex(TIMEOUT),
-      "features": getRandomArrFromStr(FEATURES),
+      "checkin": getRandomArrIndex(TIME_IN),
+      "checkout": getRandomArrIndex(TIME_OUT),
+      "features": getRandomArr(FEATURES),
       "description": `описание описание`,
-      "photos": getRandomArrFromStr(PHOTOS),
+      "photos": getRandomArr(PHOTOS),
     },
     "location": {
-      "x": getRandomInteger(0, widthMap - (OFFSET_X * 2)),
+      "x": getRandomInteger(0, (sizeOfElement.WidthMap - sizeOfElement.Pin.Width)),
       "y": getRandomInteger(130, 630),
     }
   };
 };
 
-
-const createTemplateWithData = function (template, data) {
-  let element = template.cloneNode(true);
-  element.querySelector(`img`).src = data.author.avatar;
-  element.querySelector(`img`).alt = data.offer.title;
-  element.style.left = data.location.x + OFFSET_X + `px`;
-  element.style.top = data.location.y + OFFSET_Y + `px`;
-  return element;
-};
-
-
-const renderElements = function (parent, quantity, template) {
-  for (let i = 0; i < quantity; i++) {
-    let fragment = document.createDocumentFragment();
-    fragment.appendChild(createTemplateWithData(template, getAdvert()));
-    parent.appendChild(fragment);
+const getAdverts = function () {
+  let adverts = [];
+  for (let i = 0; i < NUMBER_OF_AVATARS; i++) {
+    adverts.push(getAdvert(i));
   }
+  return adverts;
 };
 
 
-const numberOfSimilarAdvert = NUMBER_OF_AVATARS;
+const createElementsFromTemplate = function (template, data) {
+  let fragment = document.createDocumentFragment();
+  for (let i = 0; i < data.length; i++) {
+    let element = template.cloneNode(true);
+    element.querySelector(`img`).src = data[i].author.avatar;
+    element.querySelector(`img`).alt = data[i].offer.title;
+    element.style.top = data[i].location.x + (sizeOfElement.Pin.Width / 2) + `px`;
+    element.style.left = data[i].location.y + (sizeOfElement.Pin.Height / 2) + `px`;
+    fragment.appendChild(element);
+  }
+  pinMap.appendChild(fragment);
+};
+
 const pinMap = document.querySelector(`.map__pins`);
 const pinTemplate = document.querySelector(`#pin`)
     .content
     .querySelector(`.map__pin`);
 
-renderElements(
-    pinMap,
-    numberOfSimilarAdvert,
-    pinTemplate);
+createElementsFromTemplate(pinTemplate, getAdverts());
 
 document.querySelector(`.map`).classList.remove(`map--faded`);
